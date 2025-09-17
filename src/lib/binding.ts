27b552e10d202c003a5451b909c84f57c3d31ad8
Utmite <rockimunoz94@gmail.com>
2025-09-17 00:49:42 -0300

    l

--- src/lib/binding.ts
+++ src/lib/binding.ts
@@ -1,3 +1,6 @@
 "user server"
+
+import { getRequestContext } from "@cloudflare/next-on-pages"
+
 export const R2 = () => getRequestContext().env.R2
 export const R2PUBLIC = () => getRequestContext().env.R2PUBLIC
