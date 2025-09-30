bc6e78ee3f75bbe8806ac03a04b6c705f68626b7
Utmite <rockimunoz94@gmail.com>
2025-09-30 00:34:03 -0300

    l

--- migration/reload-data-sections-history-quota-etc.sh
+++ migration/reload-data-sections-history-quota-etc.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-wrangler r2 object put ousc-public/courses-descriptions.json --file=./data/courses-descriptions.json --content-t
-ype="application/json; charset=utf-8" --remote
+wrangler r2 object put ousc-public/courses-descriptions.json --file=./data/courses-descriptions.json --content-type="application/json; charset=utf-8" --remote
 
 wrangler r2 object put ousc-public/courses-sections.ndjson --file=./data/courses-sections.ndjson --content-type="application/x-ndjson; charset=utf-8" --remote
 
