4ddf05bb557f7bad27b8fa5f8a85979d042d44c0
Víctor Moreno <xdrex22@gmail.com>
2026-07-15 23:41:44 -0400

    Improve ofg handling in program curriculum display

--- src/components/curriculum/ProgramCurriculum.tsx
+++ src/components/curriculum/ProgramCurriculum.tsx
@@ -1,3 +1,4 @@
+import { customCodeParser } from "@/lib/programs";
 import type { Program } from "@/types/types";
 import Link from "next/link";
 
@@ -14,15 +15,36 @@ export default function ProgramCurriculum({ program }: ProgramCurriculumProps) {
             <h4 className="font-semibold text-sm text-center mb-3">Semestre {semester.number}</h4>
 
             <div className="grid gap-2">
-              {semester.courseCodes.map((course) => (
-                <Link
-                  key={course}
-                  href={`/${course}`}
-                  className="rounded border bg-accent px-3 py-2 text-xs hover:bg-muted"
-                >
-                  {course}
-                </Link>
-              ))}
+              {semester.courses.map((course, index) => {
+                const courseCode = semester.courseCodes[index];
+
+                if (!course?.sigle)
+                  return (
+                    <p
+                      key={`${courseCode}-${index}`}
+                      className="rounded border bg-accent font-semibold leading-tight text-center px-3 py-5 text-xs hover:bg-muted"
+                    >
+                      {customCodeParser(courseCode)}
+                    </p>
+                  );
+
+                return (
+                  <Link
+                    key={course.sigle}
+                    href={`/${course.sigle}`}
+                    className="rounded-md border bg-accent px-3 py-2 text-xs hover:bg-muted transition-colors min-w-35"
+                  >
+                    <div className="flex flex-col gap-1">
+                      <span className="font-semibold leading-tight text-center">{course.name}</span>
+
+                      <div className="flex justify-between items-center text-xs text-muted-foreground mt-2">
+                        <span>{course.sigle}</span>
+                        <span>{course.credits} cr</span>
+                      </div>
+                    </div>
+                  </Link>
+                );
+              })}
             </div>
           </div>
         ))}
--- src/data/programs/index.ts
+++ src/data/programs/index.ts
@@ -2,4 +2,6 @@ import licc from "./licc.json";
 import adp from "./administracion_publica.json";
 import { parseProgram } from "@/lib/programParser";
 
-export const programs = [parseProgram(licc), parseProgram(adp)];
+export const programs = [parseProgram(licc), parseProgram(adp)].sort((a, b) =>
+  a.name.localeCompare(b.name)
+);
