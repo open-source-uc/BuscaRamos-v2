aa4350c1475b0ae74e47a531314255e46f7413c7
Víctor Moreno <xdrex22@gmail.com>
2026-07-16 00:27:39 -0400

    Upload agronomia and actuacion programs data

--- src/data/programs/index.ts
+++ src/data/programs/index.ts
@@ -1,7 +1,12 @@
 import licc from "./licc.json";
 import adp from "./administracion_publica.json";
+import act from "./actuacion.json";
+import agr from "./agronomia.json";
 import { parseProgram } from "@/lib/programParser";
 
-export const programs = [parseProgram(licc), parseProgram(adp)].sort((a, b) =>
-  a.name.localeCompare(b.name)
-);
+export const programs = [
+  parseProgram(licc),
+  parseProgram(adp),
+  parseProgram(act),
+  parseProgram(agr),
+].sort((a, b) => a.name.localeCompare(b.name));
